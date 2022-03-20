4fb5b9a6d0cbbf651a52e93f0eb43d7e59916678
ThorrellT <thorrellt@gmail.com>
2022-03-19 20:36:49 -0400

    add contact btn hover functionality.
    have working logic for URL typing on screen when button is hovered over.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let isTyping = false;
+let isDeleting = false;
+
+
+
 $(document).ready(function () {
     $('.btn').click(function () {
         $('.nav-item').toggleClass("show");
@@ -10,12 +15,51 @@ $(document).ready(function () {
 let gh = document.getElementById('gitHub');
 let urlDisp = document.getElementById('urlDisp');
 
-gh.addEventListener('mouseover', function(){
-    urlDisp.textContent = "linkedin.com/in/";
+const shadow_t = `<span class="shadow">t</span>`
+const DISPLAY_URLS = {
+    gitHub: 'github.com/thorrell',
+    linkedIn: 'linkedin.com/in/thorrell',
+    mail: '@gmail.com'};
 
-    //code to restart animation on hoven
+function typeText(btnId) {
+    if (btnId === "mail") {
+        console.log("i read hg");
+        urlDisp.textContent = "thorrell";
+        urlDisp.insertAdjacentHTML("beforeend", shadow_t);
+        urlDisp.insertAdjacentHTML("beforeend", DISPLAY_URLS[btnId]);
+        return true;
+    }
+
+    urlDisp.textContent = DISPLAY_URLS[btnId];
+    urlDisp.insertAdjacentHTML("beforeend", shadow_t);
+}
+
+gh.addEventListener('mouseover', function(){
+
+    urlDisp.style.animationDirection = "normal";
+    console.log(DISPLAY_URLS[this["id"]]);
+
+    // urlDisp.textContent = "linkedin.com/in/thorrell";
+    // urlDisp.insertAdjacentHTML("beforeend", shadow_t);
+
+    typeText(this["id"]);
+
+    //code to restart animation on hover
     urlDisp.classList.remove("typed");
     void urlDisp.offsetWidth;
     urlDisp.classList.add("typed");
 
-  });
+});
+
+gh.addEventListener('mouseout', function(){
+
+    urlDisp.style.animationPlayState = "paused";
+    urlDisp.style.animationDirection = "reverse";
+    urlDisp.style.animationPlayState = "running";
+
+});
+
+function exchangeText() {
+    urlDisp.textContent = "linkedin.com/in/";
+}
+  
