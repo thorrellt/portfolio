806efd68be29c2a60195f9caaef6278d0ab56028
Thorrell Turner <thorrellt@gmail.com>
2022-06-05 18:58:12 -0400

    update nav-bar scrollbehaviors

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -5,15 +5,33 @@ import {Carousel} from './carousel.js';
  * NAV BAR SCROLL CONTROLS
 ****************************/
 var prevScrollpos = window.pageYOffset;
+var navItems = document.querySelectorAll('.nav-item');
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("nav-bar").style.top = "0";
   } else {
+    navItems.forEach(navItem => {
+      navItem.classList.remove("show");
+      navItem.classList.remove("hide");    
+    });
+    document.getElementById("nav-hamburger").classList.remove("hide");
     document.getElementById("nav-bar").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
 }
+
+
+
+/***************************
+ * NAV BAR HAMBURGER TOGGLE
+****************************/
+$(document).ready(function () {
+  $('.btn').click(function () {
+      $('.nav-item').toggleClass("show");
+      $('ul li').toggleClass("hide");
+  });
+});
 
 
 
